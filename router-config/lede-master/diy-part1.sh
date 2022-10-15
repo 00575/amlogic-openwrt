b66ee1c12e85dbbb1f21dbe2c64caf095ac2508a
GitHub Action <action@github.com>
2022-10-15 15:05:29 +0800

    update-2022.10.15

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -13,8 +13,6 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-# 添加第三方软件包
-git clone https://github.com/jerrykuku/luci-app-ttnode.git package/lean/luci-app-ttnode
-
-# 用第三方软件包替换当前源码库中的已有的同名软件包
+# other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+
