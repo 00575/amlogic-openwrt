a2d522b26e4772a13aae29222b7c961ee0ac682b
GitHub Action <action@github.com>
2022-12-01 01:10:46 +0800

    update-2022.12.01

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -7,13 +7,11 @@
 #========================================================================================================================
 
 # Uncomment a feed source
-sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 # sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i "1isrc-git 00575 https://github.com/00575/openwrt-packages" feeds.conf.default
-sed -i "2isrc-git ipkg https://github.com/xiangfeidexiaohuo/op-ipkg\n" feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
